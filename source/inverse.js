397044f4d4602c9a789b410769586b0fa40e0804
Mark Bykhovets <mb@hrsmnt.ru>
2020-08-29 00:49:30 +0300

    Изменил условие. При нуле и так весь массив разворачивается

--- source/inverse.js
+++ source/inverse.js
@@ -1,16 +1,16 @@
 'use strict';
 
 function inverse(array, immovable) {
-    if (immovable !== undefined) {
-        const firstElements = array.slice(0, immovable);
-        const secondElements = array.slice(immovable, array.length);
+    if (immovable !== undefined && immovable !== 0) {
+            const firstElements = array.slice(0, immovable);
+            const secondElements = array.slice(immovable, array.length);
 
-        if (immovable > 0) {
-            return [...firstElements, ...secondElements.reverse()]
-        } else if (immovable < 0) {
-            return [...firstElements.reverse(), ...secondElements]
+            if (immovable > 0) {
+                return [...firstElements, ...secondElements.reverse()]
+            } else if (immovable < 0) {
+                return [...firstElements.reverse(), ...secondElements]
+            }
         }
-    }
 
     return array.reverse();
 }
