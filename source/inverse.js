8946967c17b8bf4ea1d1c0ac1887aa9175f2f146
Mark Bykhovets <mb@hrsmnt.ru>
2020-08-29 00:54:13 +0300

    отступы

--- source/inverse.js
+++ source/inverse.js
@@ -2,15 +2,15 @@
 
 function inverse(array, immovable) {
     if (immovable !== undefined && immovable !== 0) {
-            const firstElements = array.slice(0, immovable);
-            const secondElements = array.slice(immovable, array.length);
+        const firstElements = array.slice(0, immovable);
+        const secondElements = array.slice(immovable, array.length);
 
-            if (immovable > 0) {
-                return [...firstElements, ...secondElements.reverse()]
-            } else if (immovable < 0) {
-                return [...firstElements.reverse(), ...secondElements]
-            }
+        if (immovable > 0) {
+            return [...firstElements, ...secondElements.reverse()];
+        } else if (immovable < 0) {
+            return [...firstElements.reverse(), ...secondElements];
         }
+    }
 
     return array.reverse();
 }
