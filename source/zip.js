bcab772dd9ceb3bccb3c8103130953be74a25287
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2020-08-29 14:26:35 +0300

    Add solution

---
+++ source/zip.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function zip(...args) {
+    var new_obj = {};
+
+    /* 
+
+    If we wanted to copy enumerable fields we would use this code
+
+    args.map(arg => Object.getOwnPropertyNames(arg).map(function(key) {
+        if (!new_obj.hasOwnProperty(key)) {
+            new_obj[key] = arg[key];
+        }
+    }));
+
+    */
+
+    args.map(arg => Object.keys(arg).map(function(key) {
+        if (!new_obj.hasOwnProperty(key)) {
+            new_obj[key] = arg[key];
+        }
+    }));
+
+    return new_obj;
+}
