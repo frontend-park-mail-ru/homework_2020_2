e9e813d270a8d82512cfb678cd6fd02c5802f2f1
Mark Bykhovets <mb@hrsmnt.ru>
2020-08-31 20:34:21 +0300

    Исправление оформления

--- source/inverse.js
+++ source/inverse.js
@@ -2,25 +2,33 @@
 
 const checkArray = array => {
     if (array === undefined) {
-        throw new Error("Первый аргумент обязателен");
+        throw new Error('Первый аргумент обязателен');
     }
     if (!Array.isArray(array)) {
-        throw new TypeError("Первый аргумент не массив");
+        throw new TypeError('Первый аргумент не массив');
     }
 }
 
 const checkImmovable = immovable => (
-    typeof immovable === "number" && 
+    typeof immovable === 'number' &&
     !isNaN(immovable) &&
     immovable !== 0
 );
 
+/**
+ * Переварачивает массив с возможностью оставить на месте первые/последние immovable элементы.
+ *
+ * @param {Array} array - Массив, который нужно перевернуть.
+ * @param {Number} immovable - Число непереставляемых элементов. Если immovable > 0, на месте остаются первые immovable элементы. Если immovable < 0, то immovable последние.
+ *
+ * @return {Array} Перевернутый массив, оставляя на месте указанные элементы
+ */
 const inverse = (array, immovable) => {
     checkArray(array);
     
     if (checkImmovable(immovable)) {
         if (Math.abs(immovable) > array.length) {
-            return array
+            return array;
         }
 
         const firstElements = array;
