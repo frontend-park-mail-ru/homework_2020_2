fd82b31bce946075e94064de2ae115c8bcd4fde2
Aleksandr Dergachev <smaild-m@yandex.ru>
2020-09-03 13:29:20 +0300

    Исправление ошибок после ревью

--- source/minmax.js
+++ source/minmax.js
@@ -1,17 +1,14 @@
 'use strict';
 
 const minmax = (stringOfNumbers) => {
-    if (stringOfNumbers === "")
+    if (stringOfNumbers === '') {
         return [undefined, undefined];
-    const arrayOfWords = stringOfNumbers.split(" ");
-    const arrayOfNumberWords = arrayOfWords.filter(function (number) {
-        return !isNaN(number);
-    });
-    if (arrayOfNumberWords.length === 0)
+    }
+    const arrayOfNumberWords = stringOfNumbers.split(' ').filter((number) => (!isNaN(number)));
+    if (arrayOfNumberWords.length === 0) {
         return [undefined, undefined];
-    const arrayOfNumbers = arrayOfNumberWords.map(function (number) {
-        return Number(number);
-    });
+    }
+    const arrayOfNumbers = arrayOfNumberWords.map((number) => (Number(number)));
     const min = Math.min(...arrayOfNumbers);
     const max = Math.max(...arrayOfNumbers);
     return [min, max];
