7da5db3622c1b54973f6149d1534fbc45bb7386e
Aleksandr Dergachev <smaild-m@yandex.ru>
2020-09-03 20:28:26 +0300

    Упростил логические выражения и подправил кодстайл

--- source/minmax.js
+++ source/minmax.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Finds the biggest and the smallest number in a given string.
+ * Finds the smallest and the biggest number in a given string.
  * Returns an array with 2 elements where the first element is the smallest number and the second one is the biggest.
  * Returns [undefined, undefined] if no numbers being found or the input parameter is not a string.
  * @param string - is the input string.
@@ -9,15 +9,13 @@
  */
 
 const minmax = (string) => {
-    if (typeof(string) !== 'string' || string === '') {
+    if (typeof(string) !== 'string' || !string) {
         return [undefined, undefined];
     }
-    const arrayOfNumberWords = string.split(' ').filter((number) => (!isNaN(number)));
-    if (arrayOfNumberWords.length === 0) {
+    const arrayOfNumberWords = string.split(' ').filter((number) => !isNaN(number));
+    if (!arrayOfNumberWords.length) {
         return [undefined, undefined];
     }
-    const arrayOfNumbers = arrayOfNumberWords.map((number) => (Number(number)));
-    const min = Math.min(...arrayOfNumbers);
-    const max = Math.max(...arrayOfNumbers);
-    return [min, max];
+    const arrayOfNumbers = arrayOfNumberWords.map((number) => Number(number));
+    return [Math.min(...arrayOfNumbers), Math.max(...arrayOfNumbers)];
 }
