43c9127944d7b8bed284385758ddda2f81350d13
Kudesnjk <hetchet.govno@yandex.ru>
2020-09-03 19:03:08 +0300

    Добавлены коментарии с JSDoc

--- source/set.js
+++ source/set.js
@@ -1,5 +1,12 @@
 'use strict'
 
+/**
+ * Устанавливает значение во вложенном свойстве объекта
+ * @param {object} object объект, вложенное свойство которого требуется изменить 
+ * @param {string} props строка, содержащая путь к свойству
+ * @param {*} val значение, которое требуется установить
+ * @returns {object} объект, с изменённым вложенным свойством 
+ */
 const set = (object, props, val) => {
 	const path = props.split('.')
 	path.shift()
@@ -7,6 +14,11 @@ const set = (object, props, val) => {
 		if (obj[prop] === undefined) {
             obj[prop] = {}
         }
+        /**
+         * здесь не нужен else, тк если последнего 
+         * вложенного объекта не существует, то 
+         * он будет создан, но значение не записано
+         */
 		if (idx === path.length - 1) {
             obj[prop] = val
         }
