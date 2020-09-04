13978dbdda2771bdc76335f1d3acce57284bb61c
Alexander Butolin <alexbutol@yandex.ru>
2020-09-04 14:54:16 +0300

    new algorithm

--- source/letters.js
+++ source/letters.js
@@ -5,22 +5,42 @@ let letters = (string, flag) => {
     if (string === undefined) {
         throw new SyntaxError("Не валидная строка");
     }
-    if(typeof string !== "string") {
+    if (typeof string !== "string") {
         throw new SyntaxError("Не валидная строка");
     }
 
-    let result = '';
-    for (let i = 0; i < string.length; i++) {
-        if (string.indexOf(string[i]) === string.lastIndexOf(string[i])) {
-            result += string[i];
+    string = string.split('');
+    if (flag === false)
+        string.reverse();
+
+    //Получение мапы символов
+    const letters = new Map();
+    string.forEach(element => {
+        if (letters.has(element) === false) {
+            letters.set(element, 1);
         } else {
-            if (flag === true && i === string.indexOf(string[i])) {
-                result += string[i];
-            }
-            if (flag === false && i === string.lastIndexOf(string[i])) {
-                result += string[i];
+            letters.set(element, letters.get(element) + 1);
+        }
+    })
+
+    let result = '';
+
+    if (flag === true) {
+        for (let key of letters.keys()) {
+            result += key;
+        }
+    } else if (flag === false) {
+        for (let key of letters.keys()) {
+            result += key;
+        }
+        result = result.split('').reverse().join('');
+    } else {
+        for (let key of letters.keys()) {
+            if (letters.get(key) === 1) {
+                result += key;
             }
         }
     }
+
     return result;
 }
