4ee49a6941bfb8d3b9e046c34453521ac3d05b4b
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2020-09-04 17:42:54 +0300

    Add new solution

--- source/zip.js
+++ source/zip.js
@@ -1,5 +1,22 @@
 'use strict';
 
-function zip(...args) {
+/*function zip(...args) {
     return Object.assign(...args.reverse());
+}*/
+
+function zip(...args) {
+    let new_obj = {};
+    for (let i = args.length - 1; i >= 0; i--) {
+        let obj = args[i];
+        if (typeof obj !== "object") {
+            continue;
+        }
+
+        let obj_keys = Object.keys(obj);
+        for (let j = 0; j < obj_keys.length; j++) {
+            new_obj[obj_keys[j]] = obj[obj_keys[j]];
+        }
+    }
+
+    return new_obj;
 }
