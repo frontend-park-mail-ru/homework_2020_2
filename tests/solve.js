dba9b88041109c70ab610a5134046e1d4c264d62
Myshonkov Maxim <maxim.myshonkov@gmail.com>
2020-09-05 01:51:27 +0300

    Add tests

--- tests/solve.js
+++ tests/solve.js
@@ -9,4 +9,20 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
 	});
+	QUnit.test('solve работает правильно с негативными числами', function (assert) {
+		assert.strictEqual(solve('x + -2', 1), -1);
+		assert.strictEqual(solve('-5 + x * -4 + 3', 2), -10);
+		assert.strictEqual(solve('(-1 + (-6 * -1) + -3) * x', -1), -2);
+		assert.strictEqual(solve('1 + (-x * 2) - -2 * 2', 3), -1);
+	});
+	QUnit.test('solve обрабатывает неверные входные данные', function (assert) {
+		assert.throws(() => solve(0, 0), TypeError);
+		assert.throws(() => solve('x', ""), TypeError);
+		assert.throws(() => solve("x x + 1", 1), SyntaxError);
+		assert.throws(() => solve("xx + 1", 1), SyntaxError);
+		assert.throws(() => solve("(x + ((1)", 1), SyntaxError);
+		assert.throws(() => solve("()", 1), SyntaxError);
+		assert.throws(() => solve("1 1 + 1", 1), SyntaxError);
+		assert.throws(() => solve("1 + 2 + y", 0), SyntaxError);
+	});
 });
