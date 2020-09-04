167a71c10d1e23cbf5a10def1407d72d75a47536
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2020-09-04 19:10:14 +0300

    Add function description and fix if

--- source/zip.js
+++ source/zip.js
@@ -1,8 +1,12 @@
 'use strict';
 
+/**
+ * @returns {Object} - the object with properties of all passed objects
+ * @param {...Object} - objects array
+ */
 function zip(...args) {
     return args.reverse().reduce((acc, obj) => {
-        if (typeof obj === "object") {
+        if (typeof obj === 'object' && obj !== null) {
             Object.keys(obj).forEach(key => acc[key] = obj[key]);
         }
         return acc;
