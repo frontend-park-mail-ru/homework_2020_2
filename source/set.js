75e75b0853bd9566157d0da24103ca9c073ac072
Kudesnjk <hetchet.govno@yandex.ru>
2020-09-04 21:20:12 +0300

    Исправлены замечания

--- source/set.js
+++ source/set.js
@@ -3,27 +3,22 @@
 /**
  * Устанавливает значение во вложенном свойстве объекта
  * @param {object} object объект, вложенное свойство которого требуется изменить 
- * @param {string} props строка, содержащая путь к свойству
- * @param {*} val значение, которое требуется установить
+ * @param {string} path строка, содержащая путь к свойству
+ * @param {*} value значение, которое требуется установить
  * @returns {object} объект, с изменённым вложенным свойством 
  */
-const set = (object, props, val) => {
-	const path = props.split('.')
-    path.shift()
-    let curObj = object
-	path.forEach((prop, idx) => {
-        if (curObj[prop] === undefined) {
-            curObj[prop] = {}
+const set = (object, path, value) => {
+    const properties = path.split('.').slice(1)
+    let curentObject = object
+    properties.forEach((property, index) => {
+        const isLast = index === properties.length - 1
+        if (isLast) {
+            curentObject[property] = value
         }
-        /**
-         * здесь не нужен else, тк если последнего 
-         * вложенного объекта не существует, то 
-         * он будет создан, но значение не записано
-         */
-		if (idx === path.length - 1) {
-            curObj[prop] = val
+        if (!curentObject.hasOwnProperty(property)) {
+            curentObject[property] = {}
         }
-        curObj = curObj[prop]
+        curentObject = curentObject[property]
     });
     return object
-}	
+}
