9cd0b4b3f3a92b497aba582eb32d03c55296cfff
mr-okto <maxim.mishonkov@gmail.com>
2020-09-05 02:07:12 +0300

    Fix indent

--- source/solve.js
+++ source/solve.js
@@ -5,17 +5,17 @@ const solve = (eq, x) => {
 			/(\*|\+|-){2,}/.test(eq) ||
 			/x *x/.test(eq) ||
 			/[x0-9] *[x0-9]/.test(eq)) {
-        throw new SyntaxError("Invalid expression syntax");
+		throw new SyntaxError("Invalid expression syntax");
     }
 	if (typeof eq !== "string" || typeof x !== "number" || !Number.isInteger(x)) {
-        throw new TypeError("Invalid parameters");
+		throw new TypeError("Invalid parameters");
     }
     if (eq.split("(").length != eq.split(")").length) {
-        throw new SyntaxError("Invalid braces placement");
+		throw new SyntaxError("Invalid braces placement");
     }
 	let res = new Function("x", `return ${eq}`)(x)
 	if (typeof res !== "number" || !Number.isInteger(res)) {
-        throw new SyntaxError("Invalid expression");
+		throw new SyntaxError("Invalid expression");
 	}
 	return res;
 }
