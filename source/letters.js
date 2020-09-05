69120ed52f56c93b34c2192bfbb72a6cc5d76951
SanSanchezzz <13lex04@gmail.com>
2020-09-05 13:42:16 +0300

    Add solution

---
+++ source/letters.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Функция в зависимости от значения flag возвращает различные
+ * варианты новой строки без повторяющихся символов.
+ *
+ * @param string - входная строка.
+ * @param flag - флаг, который определяет поведение программы.
+ *        Ниже перечислены различные вариации поведения программы:
+ *        1) flag = true - из строки удаляются все повторяющиеся
+ *        символы исключая первый;
+ *        2) flag = false - из строки удаляются повторяющиеся
+ *        символы исключая последний;
+ *        3) Если flag не передаётся, то из строки удаляются
+ *        все повоторяющиеся символы.
+ * @returns {string} string - новая строка без повторяющихся символов.
+ */
+const letters = (string, flag) => {
+    if (string === undefined) {
+        return 'Error';
+    }
+
+    return string.split('').filter((char, index, string) => {
+        if (flag === undefined) {
+            return string.indexOf(char) === string.lastIndexOf(char);
+        } else {
+            return flag ? string.indexOf(char) === index : string.lastIndexOf(char) === index;
+        }
+    }).join('');
+};
