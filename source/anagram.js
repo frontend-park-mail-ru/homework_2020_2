4c27f9bd7bc72cd41935d0ec6191f2d4affa1d4c
Anastasia <sanir93@gmail.com>
2020-09-05 21:04:59 +0300

    Anagram func is done

---
+++ source/anagram.js
@@ -0,0 +1,25 @@
+// Напишите функцию anagram, которая будет принимать на вход массив слов и
+// группировать его на группы слов-анаграмм. Выводить только группы из двух и
+// более слов. Слова в группах, как и сами группы, должны быть отсортированными.
+
+'use strict';
+
+const MIN_SIZE = 2;
+
+const anagram = words => {
+    let map = new Map([]);
+
+    for (let word of words) {
+        let key = word.toLowerCase().split('').sort().join('');
+
+        if (map.has(key)) {
+            map.get(key).push(word);
+        } else {
+            map.set(key, [word]);
+        }
+    }
+
+    return [...map.values()].filter(anagramPairs => anagramPairs.length >= MIN_SIZE)
+        .sort( anagramPairs => anagramPairs.sort() )
+        .sort();
+}
