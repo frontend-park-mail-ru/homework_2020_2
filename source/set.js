5d96d791ea9ae3cd36f44861ff07fe36c75442a9
Egor Aistov <egoraistov@gmail.com>
2020-09-05 23:33:28 +0300

    Функция isEmpty переписана в стрелочную, цикл for заменен на метод forEach, добавлен комментарий JSDoc, исправлены недочеты

--- source/set.js
+++ source/set.js
@@ -1,29 +1,28 @@
 'use strict';
 
-function set(object, keys, value)
-{
+/**
+ * По заданному пути находит значение объекта и меняет на новое.
+ * В случае, когда объектов, соответствующих заданным в пути, не существует,
+ * функция создает их.
+ * @param object {object} - объект, подлежащий изменению.
+ * @param keys {string} - путь, составленный из ключей объектов.
+ * @param value {*} - значение, которое необходимо задать объекту.
+ * @returns {object} - измененный объект.
+ */
+
+function set(object, keys, value) {
     let objectPart = object;
-    let path = keys.split('.');
+    const path = keys.split('.');
     path.shift();
 
-    for(let i = 0; i < path.length - 1; i++)
-    {
-        if(isEmpty(objectPart))
-            objectPart[path[i]] = {};
+    const isEmpty = (obj) => typeof obj == 'object' ? (Object.keys(obj).length === 0) : true;
 
+    path.forEach((key, i, path) => {
+        if (i === path.length - 1) return;
+        if (isEmpty(objectPart)) objectPart[path[i]] = {};
         objectPart = objectPart[path[i]];
-    }
+    });
 
     objectPart[path[path.length - 1]] = value;
     return object;
 }
-
-
-function isEmpty(obj)
-{
-    for (let key in obj)
-        return false;
-
-    return true;
-}
-
