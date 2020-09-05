b8007ef6092c09935094035449cb58270a86019b
nathanKith <ntolparov@gmail.com>
2020-09-05 22:58:21 +0300

    gotcha

---
+++ source/plain.js
@@ -0,0 +1,15 @@
+/*
+* Напишите функцию plain, которая будет принимать на вход массив массивов
+* и создавать из них один общий массив. Массивы могут быть любой вложенности
+* */
+
+const plain = arrays => {
+    while (arrays.length !== arrays.filter(item => !Array.isArray(item)).length) {
+        arrays.forEach((item, index, array) => {
+            if (Array.isArray(item)) {
+                array.splice(index, 1, ...item);
+            }
+        });
+    }
+    return arrays;
+}
