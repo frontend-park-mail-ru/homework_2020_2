d3c4c7c8890b543798a78ab195d0c2ac5de258e6
Artem <murmur@Artems-MacBook-Pro.local>
2020-09-06 13:45:01 +0300

    style fix

--- source/get.js
+++ source/get.js
@@ -8,15 +8,13 @@
  */
 const get = (object, key) => {
 	try {
-		return key.split('.').filter(element => {
-			return element;
-		}).reduce((previousValue, currentValue) => {
-			if (!previousValue) return undefined;
-			return previousValue[currentValue];
-		}, object);
+		return key.split('.')
+			.filter(element => element)
+			.reduce((previousValue, currentValue) => {
+				if (!previousValue) return undefined;
+				return previousValue[currentValue];
+			}, object);
 	} catch (e) {
-		if (e instanceof TypeError) {
-			throw new Error('Некорректный тип входных данных');
-		}
+		throw new Error('Некорректный тип входных данных');
 	}
 };
