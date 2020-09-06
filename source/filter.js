b9fdde049002fc2b8084404db095814814de6f66
Grigory <grigrol48@mail.ru>
2020-09-06 03:15:01 +0300

    add-comments

--- source/filter.js
+++ source/filter.js
@@ -1,3 +1,7 @@
+'use strict'
+
+// словарь с тем, что нужно будет экранировать
+
 const needReplace = new Map ([
     ['<', '&lt;'],
     ['>', '&gt;'],
@@ -7,15 +11,21 @@ const needReplace = new Map ([
 ]);
 
 let filter = (html, validTags) => {
+	// проверка типов входных данных
     if  (typeof html !== 'string' || !Array.isArray(validTags)) {
         return null;
     }
 
+    // ищем либо тег, либо символ
     return html.replace(/<[^<>]*>|[&"'><]/g, (symbols) => {
+    	// очищаем значение тега от <>/
         let tagValue = symbols.replace(/[<>/]/g, '');
+
+        // если не входит в список валидных, то проводим экранирование из словаря
+        // если входит, то не трогаем
         if (!validTags.includes(tagValue)) {
             return symbols.split('').map((char) => {
-                return needReplace.has(char)?needReplace.get(char):char;
+                return needReplace.has(char) ? needReplace.get(char) : char;
             }).join('');
         }
         return symbols;
