e762f28e3fd20de8e5e1b5859f1cab5566439b0d
H-b-IO-T-O-H <importantv@yandex.ru>
2020-09-06 17:17:32 +0300

    updated

--- source/euclid.js
+++ source/euclid.js
@@ -1,31 +1,72 @@
-function isValidNumber(num) {
-	return typeof num === 'number' && !isNaN(num) && num > 0;
+/** @description check input argument for non-negative, not infinite integer number
+ *  @param {number} num - input argument for validation
+ *  @return {boolean} result of check
+ **/
+const isValidNumber = num => Number.isInteger(num) && num >= 0;
+
+/** @description exceptions generator
+ *  @param err_value - object caused exception
+ *  @param {string} err_msg - error message
+ **/
+function InputArgFormatException(err_value, err_msg = 'Invalid argument format: ') {
+    this.message = err_msg + err_value;
+    this.toString = function () {
+        return this.message;
+    }
 }
 
-function two_nmbs_gcd(a, b) {
-    while (a && b)
-        a > b ? a %= b : (b %= a)
-    return a + b
+/** @description for zero arguments make infinite treatment
+ *  @param {number} a - first number for GCD
+ *  @param {number} b - second number for GCD
+ *  @return {number|Infinity} GCD for two arguments
+ **/
+const infiniteTreatment = (a, b) => {
+    if (!a && !b)
+        return Infinity;
+    return a === 0 ? b : a;
 }
 
-function input_checker(numbers) {
-     if (!numbers)
-        return null
-    let length = numbers.length
+/** @description find GCD for two validated numbers
+ *  @param {number} a - first number for GCD
+ *  @param {number} b - second number for GCD
+ *  @return {number|Infinity} GCD for two arguments
+ **/
+const twoNumbersGcd = (a, b) => {
+    if (!a || !b)
+        return infiniteTreatment(a, b);
+    while (a && b) {
+        a > b ? a %= b : b %= a;
+    }
+    return (a + b);
+}
+
+/** @description validate user input and throw exceptions by errors in input
+ * @param {[]} numbers
+ * @return {number} length of numbers list
+ **/
+const inputChecker = (numbers) => {
+    const length = numbers?.length;
+
     if (!length)
-        return null
-    for (let i = 0; i < length; ++i)
-        if (!isValidNumber(numbers[i]))
-            return null
-    return length
+        throw new InputArgFormatException(length, 'Empty sequence: length = ');
+    numbers.forEach(elem => {
+        if (!isValidNumber(elem)) throw new InputArgFormatException(elem);
+    });
+
+    return length;
 }
 
-function euclid(...numbers) {
-    let length = input_checker(numbers)
-    if (!length)
-        return null
-    let gcd_temp = numbers[0]
-    for (let i = 0; i < length - 1; ++i)
-        gcd_temp = two_nmbs_gcd(gcd_temp, numbers[i + 1])
-    return gcd_temp
+/** @description Finding GCD for any number of arguments
+ *  @param {...} numbers - sequence of numbers
+ *  @return {number} integer number - GCD of input sequence
+ **/
+const euclid = (...numbers) => {
+    const length = inputChecker(numbers);
+    let gcdTemp = numbers[0];
+
+    for (let i = 0; i < length - 1; ++i) {
+        gcdTemp = twoNumbersGcd(gcdTemp, numbers[i + 1]);
+    }
+
+    return gcdTemp;
 }
--- tests/euclid.js
+++ tests/euclid.js
@@ -25,15 +25,49 @@ QUnit.module('Тестируем функцию euclid', function () {
 		const n = 17;
 		assert.strictEqual(euclid(3 * n, 2 * n, 4 * n, 7 * n, n, 21 * n), n);
 
-		const temp = [ 80325, 55275, 8746650, 3000000, 45672375, 225, 54675 ];
-		assert.strictEqual(euclid(...[ ...temp, ...temp, ...temp, ...temp, ...temp ]), euclid(...temp));
+		const temp = [80325, 55275, 8746650, 3000000, 45672375, 225, 54675];
+		assert.strictEqual(euclid(...[...temp, ...temp, ...temp, ...temp, ...temp]), euclid(...temp));
 	});
 
+	QUnit.test('Функция должна правильно работать с нулевыми аргументами', function (assert) {
+		assert.strictEqual(euclid(0, 10, 20), 10, 'euclid(0, 10, 20) === 10');
+		assert.strictEqual(euclid(0, 666), 666, 'euclid(0, 666) === 666');
+		assert.strictEqual(euclid(0, 0, 0), Infinity, 'euclid(0, 0, 0) === Infinity');
+	})
+
 	QUnit.test('Функция должна корректно обрабатывать невалидный ввод', function (assert) {
-		assert.strictEqual(euclid(), null, 'euclid() === null');
-		assert.strictEqual(euclid('some string'), null, 'euclid(\'some string\') === null');
-		assert.strictEqual(euclid(10, 20, 30, 'some string'), null, 'euclid(10, 20, 30, \'some string\') === null');
-		assert.strictEqual(euclid(1, 2, -3, 5), null, 'euclid(1, 2, -3, 5) === null')
+		const singleInvalidArgument = [{}, null, undefined, 'some string', -1, 1.11];
+		const manyArgumentsWithInvalid = [[], [10, 20, 'some string', 30], [10, 20, -30, 40], [10, Infinity, 20]];
+		const expectedExceptionsForSingleArg = [
+			new InputArgFormatException(singleInvalidArgument[0]),
+			new InputArgFormatException(singleInvalidArgument[1]),
+			new InputArgFormatException(singleInvalidArgument[2]),
+			new InputArgFormatException(singleInvalidArgument[3]),
+			new InputArgFormatException(singleInvalidArgument[4]),
+			new InputArgFormatException(singleInvalidArgument[5]),
+		];
+		const expectedExceptionsForManyArgsWithInvalid = [
+			new InputArgFormatException(0, 'Empty sequence: length = '),
+			new InputArgFormatException('some string'),
+			new InputArgFormatException(-30),
+			new InputArgFormatException(Infinity),
+		];
+
+		expectedExceptionsForSingleArg.forEach(function (except, i, expectedExceptionsForSingleArg) {
+			assert.throws(
+				() => { euclid(singleInvalidArgument[i]); },
+				except,
+				"Check for single invalid argument exception raising..."
+			);
+		});
+
+		expectedExceptionsForManyArgsWithInvalid.forEach(function (except, i, expectedExceptionsForManyArgsWithInvalid) {
+			assert.throws(
+				() => { euclid(...manyArgumentsWithInvalid[i]); },
+				except,
+				"Check for invalid argument exception raising in list..."
+			);
+		});
 	});
 
 });
