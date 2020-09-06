7730e29f73a9dd1ab65ca386a9706adef29770f1
Karyagin <faceit_accaunt@mail.ru>
2020-09-06 12:48:38 +0300

    chaining fixed

--- source/sort.js
+++ source/sort.js
@@ -9,18 +9,20 @@
  * @return {string} text, Преобразованная строка.
  */
 const sort = text => {
-
     if (typeof (text) !== "string") {
         throw new SyntaxError("incorrect type entered");
     }
 
     const collator = new Intl.Collator();
 
+    const charcompare = (a, b) => collator.compare(a, b);
+
     let arrayText = text.split(' ');
 
-    arrayText = arrayText.map((word) => word.toLowerCase().split('').sort((a, b) => collator.compare(a, b)).join(''));
+    arrayText = arrayText.map((word) => word.toLowerCase().split('')
+                         .sort((a, b) => charcompare(a, b)).join(''));
 
-    arrayText.sort((a, b) => collator.compare(a, b))
+    arrayText.sort((a, b) => charcompare(a, b))
 
     arrayText = arrayText.map((word) => word[0].toUpperCase() + word.substring(1));
 
