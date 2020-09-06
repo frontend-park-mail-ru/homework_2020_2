b5ecc4d5cdcbab887d21995749710b5fa808fd95
Amaterasy17 <sheb70017@gmail.com>
2020-09-06 14:45:35 +0300

    I have fixed style code

--- source/minmax.js
+++ source/minmax.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const minmax = string => {
-    let massive = string.split(/\ |, /).filter(item => !isNaN(parseFloat(item)));
-    let result = massive.filter(item =>
+const minmax = (string) => {
+    const massive = string.split(/\ |, /).filter((item) => !isNaN(parseFloat(item)));
+    const result = massive.filter((item) =>
         item == Math.max(...massive) || item == Math.min(...massive)
-    ).length == 0 ? [undefined, undefined] : [Math.min(...massive), Math.max(...massive)];
+    ).length === 0 ? [undefined, undefined] : [Math.min(...massive), Math.max(...massive)];
     return result;
 }
