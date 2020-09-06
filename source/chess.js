2ee6d0900a4a6fb5c53e30076872f79334f95a96
Sergei Alexeev <snalexeev@outlook.com>
2020-09-06 14:34:28 +0300

    Исправления после второй проверки кода
    Проверка на валидность вынесена в отдельную функцию, избавился от циклов for.

--- source/chess.js
+++ source/chess.js
@@ -3,31 +3,43 @@
 /**
  * @function
  * @param number
+ * @return {Boolean} - result of checking
+ */
+const isInteger = (number) => {
+    if (!isFinite(number) || number <= 1 || !Number.isInteger(+number)) {
+        return false;
+    }
+    return true;
+}
+
+
+
+/**
+ * @function
+ * @param number
  * @return {string|null} - chess line drawing or null
  */
 const chess = (number) => {
     // проверка данных на вход
-    if (!isFinite(number) || number <= 1 || !Number.isInteger(Number(number))) {
+    if (!isInteger(number)) {
         return null;
     }
 
-    let result = '';
-    let line = ''
-    let symbolSelection;
-    let odd = number % 2 == 1;
-    for (let i = 0; i < number; ++i) {
-        symbolSelection = i % 2 === 0;
-        if (symbolSelection) {
-            line = "* ".repeat(number / 2);
-        } else {
-            line = " *".repeat(number / 2);
-        }
-        if (odd) {
-            line += line.charAt(0);
-        }
-        symbolSelection = !symbolSelection;
-        result += (line + '\n');
+    let result;
+    let firstLine, secondLine;
+    let odd = number % 2;
+    firstLine = "* ".repeat(number / 2);
+    secondLine = " *".repeat(number / 2);
+    if (odd) {
+        firstLine += firstLine[0];
+        secondLine += secondLine[0];
     }
+    firstLine += '\n';
+    secondLine += '\n';
 
+    result = (firstLine + secondLine).repeat(number / 2);
+    if (odd) {
+        result += firstLine;
+    }
     return result;
 }
