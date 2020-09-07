4b993685bf88f28e0fd345f322e278d0f91fa017
pringleskate <kattyseskimojoe@gmail.com>
2020-09-07 17:45:07 +0300

    Добавила reduce вместо forEach

--- source/roman.js
+++ source/roman.js
@@ -4,7 +4,7 @@
  * Оберточная функция перевода чисел
  *
  * @param {number|string} number - введенное пользователем число для перевода
- * @returns {number}
+ * @returns {number|string}
  */
 const roman = (number) => {
     if (/^[IVXLCDMivxlcdm]+$/.test(number)) {
@@ -25,21 +25,21 @@ const deromanize = (number) => {
     const ROMAN_ALPHABET = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
     const DIGITS = Object.keys(ROMAN_ALPHABET);
 
-    let res = 0;
-    number.toUpperCase().split('').forEach((element, index) => {
-        (DIGITS.indexOf(element) < DIGITS.indexOf(number.charAt(index + 1).toUpperCase())) ?
-            res -= ROMAN_ALPHABET[element] :
-            res += ROMAN_ALPHABET[element];
-    });
-
-    return res;
+    return number.toUpperCase()
+            .split('')
+            .reduce((result, current, index) => {
+                    (DIGITS.indexOf(current) < DIGITS.indexOf(number.charAt(index + 1).toUpperCase())) ?
+                        result -= ROMAN_ALPHABET[current] :
+                        result += ROMAN_ALPHABET[current];
+                    return result;
+            }, 0);
 }
 
 /**
  * Функция перевода арабского числа в римское
  *
  * @param {number} number - введенное пользователем число для перевода
- * @returns {number}
+ * @returns {string}
  */
 const romanize = (number) => {
     if (number > 3999 || number === 0) {
@@ -47,14 +47,12 @@ const romanize = (number) => {
     }
 
     const ROMAN_ALPHABET = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
-    let final_str = '';
 
-    Object.keys(ROMAN_ALPHABET).forEach((element) => {
-        const QUOTIENT = Math.floor(number / ROMAN_ALPHABET[element]);
+    return Object.keys(ROMAN_ALPHABET).
+                reduce((result_string, current, index) => {
+                        const QUOTIENT = Math.floor(number / ROMAN_ALPHABET[current]);
 
-        number -= QUOTIENT * ROMAN_ALPHABET[element];
-        final_str += element.repeat(QUOTIENT);
-    });
-
-    return final_str;
+                        number -= QUOTIENT * ROMAN_ALPHABET[current];
+                        return result_string + current.repeat(QUOTIENT);
+                }, '');
 }
