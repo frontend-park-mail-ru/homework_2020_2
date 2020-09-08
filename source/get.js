3ef75cea44f7f0b345b865677f2360752749097a
arugaf <arug113@gmail.com>
2020-09-08 11:53:55 +0300

    code style fixed (indents and argument braces)

--- source/get.js
+++ source/get.js
@@ -1,6 +1,6 @@
 'use strict';
 
 const get = (object, property) => {
-    return property.split('.').filter(str => str).reduce((result, key) =>
+    return property.split('.').filter((str) => str).reduce((result, key) =>
         (result && result[key] ? result[key] : undefined), object);
 };
