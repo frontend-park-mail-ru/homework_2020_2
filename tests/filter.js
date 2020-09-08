ffe257684ca191b4252aca30bf0e22f503140b4b
Mox1e <oleg22338@gmail.com>
2020-09-08 19:58:19 +0300

    add test for other symbols (like for <)

--- tests/filter.js
+++ tests/filter.js
@@ -22,6 +22,16 @@ QUnit.module('Проверка работы функции filter', function () 
 		assert.strictEqual(output, expected);
 	});
 
+	QUnit.test('filter не экранирует валидные html-тэги, но экранирует символы', function (assert) {
+		const input = '<strong>Hello, <em>World!</em></strong> 1 + 2 < 4! & 4 > 3';
+
+		const output = filter(input, [ 'strong', 'em' ]);
+
+		const expected = '<strong>Hello, <em>World!</em></strong> 1 + 2 &lt; 4! &amp; 4 &gt; 3';
+
+		assert.strictEqual(output, expected);
+	});
+
 	QUnit.test('filter экранирует XSS', function (assert) {
 		assert.strictEqual(filter(`<script>alert('1');</script>`, [ 'strong', 'em' ]), '&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;');
 		assert.strictEqual(filter(`<img src="bad" onerror="alert('1');">`, [ 'strong', 'em' ]), '&lt;img src=&quot;bad&quot; onerror=&quot;alert(&#39;1&#39;);&quot;&gt;');
