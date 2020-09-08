a7d2856f41b678f37b31e2869ed8a5355f1ac8cd
marksadykov <sadykov.mark@gmail.com>
2020-09-09 00:59:44 +0300

    simplification

--- source/format.js
+++ source/format.js
@@ -1,41 +1,56 @@
 'use strict';
 
 const format = (inputArray, columns) => {
-    if (!(Array.isArray(inputArray)) || (typeof (columns) !== 'number'))
-        throw new SyntaxError('wrong type');
+    if (!(Array.isArray(inputArray)) || (typeof columns !== 'number')
+        || (!(Number.isFinite(columns))) || (Number.isNaN(columns)) || (!((columns ^ 0) === columns))) {
+        throw new TypeError('wrong type');
+
+    }
+
+    if ((inputArray.length == 0) || (!(inputArray.every(item => (item ^ 0) === item)))) {
+        throw new TypeError('wrong type');
+    }
+
+    if (columns === 0) {
+        return '';
+    }
 
     const line = inputArray.slice(0);
-    const table = [];
-    const rows = Math.ceil(line.length / columns);
 
-    for (let i = 0; i < rows; i++) {
-        let rowArray = [];
-        for (let j = 0; j < columns; j++) {
-            let current = String(line.shift());
-            (current === 'undefined') ? rowArray.push('') : rowArray.push(current);
+    const columnsSizes = Array(columns);
+    for (let i = 0; i < line.length; i++) {
+        const columnNumber = i % columns;
+        if (columnsSizes[columnNumber] === undefined) {
+            columnsSizes[columnNumber] = 0;
         }
-        table.push(rowArray);
+        const currentMax = columnsSizes[columnNumber];
+        const currentStr = String(line[i]);
+        if ((currentStr !== 'undefined') && (currentStr.length > currentMax)) {
+            columnsSizes[columnNumber] = currentStr.length;
+        }
     }
 
-    for (let i = 0; i < columns; i++) {
-        let col = table.map(x => x[i]);
-        let lenRow  = col.reduce((a, b) => { return a.length > b.length ? a : b; });
-        lenRow = (lenRow !== undefined) ? lenRow.length : 0;
+    let result = '';
+    for (let i = 0; i < line.length; i++){
+         const columnNumber = i % columns;
+         let current = '';
+         if (String(line[i]) === 'undefined') {
+             current =''.toString().padStart(columnsSizes[columnNumber]);
+         }
+         else {
+             current = line[i].toString().padStart(columnsSizes[columnNumber]);
+         }
+         result += current;
 
-        col.forEach((item, j) => {
-            let current = table[j][i];
-            table[j][i] = '';
-            (!((current === '') && (j === col.length - 1) && (i === columns - 1)))
-                ? table[j][i] += ' '.repeat(lenRow - current.length) : table[j][i] = '';
-            table[j][i] += current;
-        });
+        if ((columns === (columnNumber + 1)) && (i !== line.length - 1)){
+            result += '\n';
+        }
+        else {
+            if (i !== line.length - 1) {
+                result += ' ';
+            }
+        }
     }
 
-    let print = '';
-    table.forEach((item, i) => {
-        let current = '';
-        current = (i === rows - 1) ? item.join(' ') : item.join(' ') + '\n';
-        print += current;
-    });
-    return print.trimEnd();
-};
+    return result;
+}
--- tests/format.js
+++ tests/format.js
@@ -3,7 +3,7 @@
 QUnit.module('Тестируем функцию format', function () {
 	QUnit.test('format работает правильно c одной колонкой и положительными числами', function (assert) {
 		const input1 = [ 0, 1, 2, 10, 100, 1000, 10000 ];
-		const input2 = [ 10000000, 1, 20000000000, 10, 100, 1000, 10000 ];
+		const input2 = [ 1000000, 1, 20000000, 10, 100, 1000, 10000 ];
 
 		const expected1 =
 			'    0\n' +
@@ -15,13 +15,13 @@ QUnit.module('Тестируем функцию format', function () {
 			'10000';
 
 		const expected2 =
-			'   10000000\n' +
-			'          1\n' +
-			'20000000000\n' +
-			'         10\n' +
-			'        100\n' +
-			'       1000\n' +
-			'      10000';
+			' 1000000\n' +
+			'       1\n' +
+			'20000000\n' +
+			'      10\n' +
+			'     100\n' +
+			'    1000\n' +
+			'   10000';
 
 		assert.strictEqual(format(input1, 1), expected1);
 		assert.strictEqual(format(input2, 1), expected2);
@@ -162,24 +162,56 @@ QUnit.module('Тестируем функцию format', function () {
 	QUnit.test('format работает правильно, если в качестве исходных данных для вывода подается не массив', function (assert) {
 		const input = 100;
 
-		const expected = SyntaxError('wrong type');
+		const expected = TypeError('wrong type');
 
 		assert.throws(() => format(input, 2), expected);
 	});
 
-	QUnit.test('format работает правильно, если в качестве количества колонок подается не число', function (assert) {
+	QUnit.test('format работает правильно, если в качестве количества колонок подается не целое число', function (assert) {
 		const input = [0, 1];
 
-		const expected = SyntaxError('wrong type');
+		const expected = TypeError('wrong type');
 
 		assert.throws(() => format(input, '2'), expected);
+		assert.throws(() => format(input, Infinity), expected);
+		assert.throws(() => format(input, NaN), expected);
+		assert.throws(() => format(input, 3.5), expected);
+		assert.throws(() => format(input, -3.5), expected);
 	});
 
 	QUnit.test('format работает правильно, если оба аргумента неправильного типа', function (assert) {
 		const input = 100;
 
-		const expected = SyntaxError('wrong type');
+		const expected = TypeError('wrong type');
 
 		assert.throws(() => format(input, '2'), expected);
+	});
+
+	QUnit.test('format работает правильно, если подается пустой массив', function (assert) {
+		const input = [];
+
+		const expected = TypeError('wrong type');
+
+		assert.throws(() => format(input, 2), expected);
+	});
+
+	QUnit.test('format работает правильно, если в массиве есть нецелые числа или другие типы данных', function (assert) {
+		const input1 = [ -1.5, 4];
+		const input2 = [ 't', 4];
+		const input3 = [ {}, {} ];
+
+		const expected = TypeError('wrong type');
+
+		assert.throws(() => format(input1, 2), expected);
+		assert.throws(() => format(input2, 2), expected);
+		assert.throws(() => format(input3, 2), expected);
+	});
+
+	QUnit.test('format работает правильно, если количество колонок равно 0', function (assert) {
+		const input = [ 0, 1, 2, 10, 100, -100, 1000, 10000, -10000 ];
+
+		const expected = '';
+
+		assert.strictEqual(format(input, 0), expected);
 	});
 });
