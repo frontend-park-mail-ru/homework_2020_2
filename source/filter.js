6b761fd5f7257eb2ef8401d0201fbdfdfa20bc7b
Mox1e <oleg22338@gmail.com>
2020-09-08 19:52:02 +0300

    define regular expressions

--- source/filter.js
+++ source/filter.js
@@ -8,6 +8,10 @@ const HTML_ENTITY_MAP = {
     "'": '&#39;',
 };
 
+const TAGS_OR_SYMBOLS_RE = /<[^<>]+>|[<&"'>]/g;
+const GET_TAG_RE = /[<>/]/g;
+const FIND_SYMBOLS_RE = /[<&"'>]/g;
+
 /**
  * filters html code, leaving only allowed html tags.
  * @param {string} input - html code
@@ -18,11 +22,11 @@ let filter = (input, tagsArray) => {
         alert("Type of input should be string.");
         return null;
     }
-
-    return input.replace(/<[^<>]+>|[<&"'>]/g, (match) => {
-        const currentTag = match.replace(/[<>/]/g, '');
+    
+    return input.replace(TAGS_OR_SYMBOLS_RE, (match) => {
+        const currentTag = match.replace(GET_TAG_RE, '');
         return tagsArray.includes(currentTag) ? match : 
-            match.replace(/[<&"'>]/g, (match) => HTML_ENTITY_MAP[match]);
+            match.replace(FIND_SYMBOLS_RE, (match) => HTML_ENTITY_MAP[match]);
     });
 };
 
