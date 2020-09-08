7d9f24918974e70d46a591dff2ed674420080422
Dellvin <dellvin.black@gmail.com>
2020-09-08 10:18:34 +0300

    made rle func and add 3 tests

--- source/max.js
+++ source/max.js
@@ -1,3 +1,28 @@
 'use strict';
 
 const max = numbers => Math.max(...numbers);
+
+function rle(originData) {
+    let compressedData="";
+    if(typeof originData!=="string")return "NOT STRING!";
+    let counter = 1, prevSymbol=originData.charAt(0);
+    for (let i=1; i<originData.length; i++) {
+        if (!isNaN(originData.charAt(i))) return "UNEXPECTED SYMBOL!";
+        if (originData.charAt(i) === prevSymbol) {
+            counter++;
+        } else {
+            compressedData=addToCompressed(compressedData, prevSymbol, counter);
+            counter = 1;
+            prevSymbol=originData.charAt(i);
+        }
+    }
+    compressedData=addToCompressed(compressedData, prevSymbol, counter)
+    return compressedData;
+}
+
+function addToCompressed(compressed, symbol, counter){
+    compressed += symbol;
+    if(counter!==1)
+        compressed += counter;
+    return compressed;
+}
