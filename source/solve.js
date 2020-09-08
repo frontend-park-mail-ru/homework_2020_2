3d5ea79e42804b47a6bf92db20d34c87500c76a7
Felix1Green <vvnncc@mail.ru>
2020-09-08 19:36:53 +0300

    Replaced eval with new Function. Some tests were reconfigured

--- source/solve.js
+++ source/solve.js
@@ -1,12 +1,26 @@
-"use strict";
+'use strict';
 
-const MATH_EXPRESSION_VARIABLE = 'x';
 const MATH_MARKING_REGEXP = /^\s*-?[(0-9x]+[(0-9x*+-\/)\s]*$/;
+const INVALID_MATH_EXPRESSION = 'invalid math expression';
+const WRONG_FUNCTION_PARAMS = 'incorrect function input';
 
-function solve(expression, variable){
-    const mathExpression = String(expression);
-    if(mathExpression && mathExpression.match(MATH_MARKING_REGEXP)){
-        return eval(mathExpression.replaceAll(MATH_EXPRESSION_VARIABLE, variable));
+
+/**
+ * Calculates math expression with signs +-* and one unknown variable x
+ * @param {String} expression - Math expression
+ * @param {Number} variable - Unknown Math variable
+ * @returns {Number} - Calculated math expression value
+ * @throws {TypeError} - types of function inputs are wrong
+ * @throws {SyntaxError} - Math expression contains unsupported signs or incorrect
+ */
+const solve = (expression, variable) => {
+    if(typeof(expression) !== 'string' ||  typeof(variable) !== 'number'){
+        throw new TypeError(WRONG_FUNCTION_PARAMS);
     }
-    return null;
+
+    if(!expression.match(MATH_MARKING_REGEXP)){
+        throw new SyntaxError(INVALID_MATH_EXPRESSION);
+    }
+
+    return new Function('x',`return ${expression}`)(variable);
 }
--- tests/solve.js
+++ tests/solve.js
@@ -13,12 +13,20 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('   1 -  2  -3 + 4', 1), 0);
 	});
 
-	QUnit.test('solve возвращает корректный ответ при неверном выражении', function (assert){
-		assert.strictEqual(solve('abcx', 1), null);
-		assert.strictEqual(solve('x + 5a + 3b + x^2', 1), null);
-		assert.strictEqual(solve('x + x^2', 1), null);
-		assert.strictEqual(solve('4 + 3 + 2 + null', 1), null);
-		assert.strictEqual(solve('a + b + c', 1), null);
+	QUnit.test('solve отвечает корректной ошибкой при неверном выражении', function (assert){
+		assert.throws(() => solve('abcx',1), new SyntaxError(INVALID_MATH_EXPRESSION));
+		assert.throws(() => solve('x + 5a + 3b + x^2',1), new SyntaxError(INVALID_MATH_EXPRESSION));
+		assert.throws(() => solve('x + x^2',1), new SyntaxError(INVALID_MATH_EXPRESSION));
+		assert.throws(() => solve('4 + 3 + 2 + null',1), new SyntaxError(INVALID_MATH_EXPRESSION));
+		assert.throws(() => solve('a + b + c',1), new SyntaxError(INVALID_MATH_EXPRESSION));
+	});
+
+	QUnit.test('solve отвечает корректной ошибкой при неправильном типе входных аргументов', function (assert){
+		assert.throws(() => solve(1,1), new TypeError(WRONG_FUNCTION_PARAMS));
+		assert.throws(() => solve("asd","asd"), new TypeError(WRONG_FUNCTION_PARAMS));
+		assert.throws(() => solve(1,"asd"), new TypeError(WRONG_FUNCTION_PARAMS));
 	})
+
+
 
 });
