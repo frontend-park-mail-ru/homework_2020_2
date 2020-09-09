a9eb64cbaf00ff1d96b7ce82436bccde59969bd9
Amaterasy17 <sheb70017@gmail.com>
2020-09-09 19:54:25 +0300

    Fix in style code

--- source/minmax.js
+++ source/minmax.js
@@ -1,15 +1,20 @@
 'use strict'
+
 /**
  * Function returns massive of 2 elements, where first - min, second - max items from current string
  * @param string - string or number
  * @returns {(Array)}
  */
+
 const minmax = (string) => {
-    if (typeof (string) !== "string")
+    if (typeof string !== 'string') {
         return [undefined, undefined];
+    }
+
     const massive = string.split(/ |, /).filter((item) => !isNaN(parseFloat(item)));
+
     const min = Math.min(...massive);
     const max = Math.max(...massive);
-    const result = (massive.length === 0) ? [undefined, undefined] : [min, max];
-    return result;
+
+    return (massive.length === 0) ? [undefined, undefined] : [min, max];
 }
