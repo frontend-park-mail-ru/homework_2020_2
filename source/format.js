ba284950d5fb0a75caaf302bf26348982c620078
Timofei Makarov <macintosh1999@mail.ru>
2020-09-09 16:52:02 +0300

    corrected most errors

--- source/format.js
+++ source/format.js
@@ -7,45 +7,32 @@
  * @returns {String}
  */
 
-
 const format = (array, columns) => {
-	if (!Array.isArray(array) || columns <= 0 || columns === undefined) {
-    	throw Error('Некорректный тип входных данных');
+	if (!Array.isArray(array) || columns <= 0 || typeof(columns) !== 'number') {
+		throw Error('Некорректный тип входных данных');
 	}
 
 	// Calculating max length of number in each column
-	let colLength = new Array(columns);
-	colLength.fill(0, 0);
+	let colLength = new Array(columns).fill(0);
+	array = array.filter(elem => typeof(elem) === 'number');
 
-	const filteredArray = array.filter(elem=> typeof(elem) != 'undefined');
+	for (let i = 0; i < array.length; i++) {
+		const colNum = i % columns;
 
-	for (let i = 0; i < columns; i++) {
-		let column = [];
-
-		for (let j = i; j < filteredArray.length; j += columns) {
-				column.push(String(filteredArray[j]));
-		}
-
-		colLength[i] = column.reduce(function(maxLength, current) {
-			const currentLength = current.length;
-
-			return maxLength < currentLength ?
-				maxLength = currentLength : maxLength;
-		}, 0);
+		colLength[colNum] = Math.max(colLength[colNum], String(array[i]).length);
 	}
 
 	// Creating formated array
-	const arraySize = filteredArray.length;
 	let table = '';
 
-	for (let i = 0; i < arraySize; i++) {
+	for (let i = 0; i < array.length; i++) {
 		const colNum = i % columns;
 
 		if (colNum) {
 			table += ' ';
 		}
-		table += String(filteredArray[i]).padStart(colLength[colNum],' ');
-		if (i !== arraySize - 1 && colNum === columns - 1) {
+		table += String(array[i]).padStart(colLength[colNum],' ');
+		if (i !== array.length - 1 && colNum === columns - 1) {
 			table += '\n';
 		}
 	}
