7e84b99d31e3a1856ef313328aefc41e3b5646dc
Pavel <pavel_smir@mail.ru>
2020-09-09 13:16:18 +0300

    Edited style

--- source/chess.js
+++ source/chess.js
@@ -2,27 +2,26 @@
 
 
 /**
- * Returns the chess board n by n in string
- * @param {number} n, if n is not a number or n <= 1, functioon returns null
+ * Returns the chess board size by size in string
+ * @param {number} size, if size is not a number or size <= 1, functioon returns null
  * @returns {(string|null)} String which contains the chess board or null
  */
-const chess = (n) => {
-	if (isNaN(+n) || n <= 1 || !Number.isInteger(+n)) {
-		return null;
-	}
+const chess = (size) => {
+    if (isNaN(+size) || size <= 1 || !Number.isInteger(+size)) {
+        return null;
+    }
 
-	return (makeChessLine(n, true) + makeChessLine(n, false)).repeat(n / 2) +
-		 (n % 2 ? makeChessLine(n, true) : '');
+    return (makeChessLine(size, true) + makeChessLine(size, false)).repeat(size / 2) +
+        (size % 2 ? makeChessLine(size, true) : '');
 };
-
 
 
 /**
  * Returns the chess board line, odd or even
- * @param {number} n, line length
+ * @param {number} length, line length
  * @param {boolean} isEven, if isEven is true, even line is returned, otherwise, odd line is returned 
  * @returns {string} String which contains the chess board line
  */
- const makeChessLine = (n, isEven) => isEven ? 
- 	'* '.repeat(n / 2) + (n % 2 ? '*\n' : '\n') :
- 	' *'.repeat(n / 2) + (n % 2 ? ' \n' : '\n');
+const makeChessLine = (length, isEven) => isEven ?
+    '* '.repeat(length / 2) + (length % 2 ? '*\n' : '\n') :
+    ' *'.repeat(length / 2) + (length % 2 ? ' \n' : '\n');
