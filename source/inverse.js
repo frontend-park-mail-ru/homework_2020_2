fc98d68011c18642115195d9031c88d890888e17
Vladislav <shush2626@mail.ru>
2020-09-09 21:03:06 +0300

    fix inverse

--- source/inverse.js
+++ source/inverse.js
@@ -1,21 +1,18 @@
 'use strict';
 
 const inverse = function (arr, to = 0) {
-    let res = [];
+    if(typeof arr != 'object' || typeof to != 'number') {
+        return arr;
+    }
+    let res = []
     if (to >= 0) {
-        while (arr.length && to != 0) {
-            res.push(arr.shift());
-            to--;
-        }
-        while (arr.length)
-            res.push(arr.pop());
+        let splice_arr = arr.splice(0, to);
+        let reverse_arr = arr.reverse();
+        res = splice_arr.concat(reverse_arr);
     } else {
-        while (arr.length && to != 0) {
-            res.unshift(arr.pop());
-            to++;
-        }
-        while (arr.length)
-            res.unshift(arr.shift());
+        let splice_arr = arr.splice(to, Math.abs(to));
+        let reverse_arr = arr.reverse();
+        res = reverse_arr.concat(splice_arr);
     }
     return res;
 }
