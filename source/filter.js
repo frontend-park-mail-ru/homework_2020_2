117762cdbf23e422d259ebbea72fdf51723099d2
Grigory <grigrol48@mail.ru>
2020-09-09 20:54:46 +0300

    add-exceptions-and-tests

--- source/filter.js
+++ source/filter.js
@@ -1,7 +1,5 @@
 'use strict'
-
-// словарь с тем, что нужно будет экранировать
-
+ 
 const needReplace = new Map ([
     ['<', '&lt;'],
     ['>', '&gt;'],
@@ -9,24 +7,31 @@ const needReplace = new Map ([
     ['\'', '&#39;'],
     ['"', '&quot;']
 ]);
-
-let filter = (html, validTags) => {
-	// проверка типов входных данных
-    if  (typeof html !== 'string' || !Array.isArray(validTags)) {
-        return null;
+ 
+/**
+ * Escapes invalid html html tags.
+ * @param {string} html - String with html tags or symbols.
+ * @param {string[]} validTags - Tags allowed to use.
+ * @returns {string} Filtered string.
+ */
+ 
+const filter = (html, validTags) => {
+    if  (typeof html !== 'string') {
+        throw Error('First arg must be a string!')
     }
-
-    // ищем либо тег, либо символ
+ 
+    if (!Array.isArray(validTags)) {
+        throw Error('Second arg must be an Array')
+    }
+ 
+    if (validTags.some((element) => typeof element !== 'string')) {
+        throw Error('Must be only Array of strings')
+    }
+ 
     return html.replace(/<[^<>]*>|[&"'><]/g, (symbols) => {
-    	// очищаем значение тега от <>/
-        let tagValue = symbols.replace(/[<>/]/g, '');
-
-        // если не входит в список валидных, то проводим экранирование из словаря
-        // если входит, то не трогаем
+        const tagValue = symbols.replace(/[<>/]/g, '');
         if (!validTags.includes(tagValue)) {
-            return symbols.split('').map((char) => {
-                return needReplace.has(char) ? needReplace.get(char) : char;
-            }).join('');
+            return symbols.split('').map((char) => needReplace.get(char) || char).join('');
         }
         return symbols;
     });
--- tests/filter.js
+++ tests/filter.js
@@ -1,59 +1,58 @@
 'use strict';
-
+ 
 QUnit.module('Проверка работы функции filter', function () {
-	QUnit.test('filter экранирует символы в обычном тексте', function (assert) {
-		const input = '- "42!", сказала Машина. Это и был главный ответ на Вопрос жизни, вселенной & всего такого...';
-
-		const output = filter(input, [ 'strong', 'em' ]);
-
-		const expected = '- &quot;42!&quot;, сказала Машина. Это и был главный ответ на Вопрос жизни, вселенной &amp; всего такого...';
-
-		assert.strictEqual(output, expected);
-	});
-
-	QUnit.test('filter не экранирует валидные html-тэги', function (assert) {
-		const input = '<strong>Hello, <em>World!</em></strong> 1 + 2 < 4!';
-
-		const output = filter(input, [ 'strong', 'em' ]);
-
-		const expected = '<strong>Hello, <em>World!</em></strong> 1 + 2 &lt; 4!';
-
-		assert.strictEqual(output, expected);
-	});
-
-	QUnit.test('filter экранирует XSS', function (assert) {
-		assert.strictEqual(filter(`<script>alert('1');</script>`, [ 'strong', 'em' ]), '&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;');
-		assert.strictEqual(filter(`<img src="bad" onerror="alert('1');">`, [ 'strong', 'em' ]), '&lt;img src=&quot;bad&quot; onerror=&quot;alert(&#39;1&#39;);&quot;&gt;');
-	});
-
-	QUnit.test('filter экранирует последовательность левых или правых скобок, а так же пустые теги', function (assert) {
-		const input = '<<<><script> здесь находится последовательность символов и &, ", дальше -- пустые теги <> <<>></script>';
-
-		const output = filter(input, ['script']);
-
-		const expected = '&lt;&lt;&lt;&gt;<script> здесь находится последовательность символов и &amp;, &quot;, дальше -- пустые теги &lt;&gt; &lt;&lt;&gt;&gt;</script>';
-
-		assert.strictEqual(output, expected);
-	});
-
-	QUnit.test('filter выдает null при неправильном аргументе слева', function (assert) {
-		const input = 123456;
-
-		const output = filter(input, [ 'script', 'xss' ]);
-
-		const expected = null;
-
-		assert.strictEqual(output, expected);
-	});
-
-	QUnit.test('filter выдает null при неправильном аргументе справа', function (assert) {
-		const input = 'какая-то строка';
-
-		const output = filter(input, 123456);
-
-		const expected = null;
-
-		assert.strictEqual(output, expected);
-	});
-
+    QUnit.test('filter экранирует символы в обычном тексте', function (assert) {
+        const input = '- "42!", сказала Машина. Это и был главный ответ на Вопрос жизни, вселенной & всего такого...';
+ 
+        const output = filter(input, [ 'strong', 'em' ]);
+ 
+        const expected = '- &quot;42!&quot;, сказала Машина. Это и был главный ответ на Вопрос жизни, вселенной &amp; всего такого...';
+ 
+        assert.strictEqual(output, expected);
+    });
+ 
+    QUnit.test('filter не экранирует валидные html-тэги', function (assert) {
+        const input = '<strong>Hello, <em>World!</em></strong> 1 + 2 < 4!';
+ 
+        const output = filter(input, [ 'strong', 'em' ]);
+ 
+        const expected = '<strong>Hello, <em>World!</em></strong> 1 + 2 &lt; 4!';
+ 
+        assert.strictEqual(output, expected);
+    });
+ 
+    QUnit.test('filter экранирует XSS', function (assert) {
+        assert.strictEqual(filter(`<script>alert('1');</script>`, [ 'strong', 'em' ]), '&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;');
+        assert.strictEqual(filter(`<img src="bad" onerror="alert('1');">`, [ 'strong', 'em' ]), '&lt;img src=&quot;bad&quot; onerror=&quot;alert(&#39;1&#39;);&quot;&gt;');
+    });
+ 
+    QUnit.test('filter экранирует последовательность левых или правых скобок, а так же пустые теги', function (assert) {
+        const input = '<<<><script> здесь находится последовательность символов и &, ", дальше -- пустые теги <> <<>></script>';
+ 
+        const output = filter(input, ['script']);
+ 
+        const expected = '&lt;&lt;&lt;&gt;<script> здесь находится последовательность символов и &amp;, &quot;, дальше -- пустые теги &lt;&gt; &lt;&lt;&gt;&gt;</script>';
+ 
+        assert.strictEqual(output, expected);
+    });
+ 
+    QUnit.test('filter выдает исключение при неправильном аргументе слева', function (assert) {
+        const input = 123456;
+ 
+        assert.throws(() => filter(input, [ 'script', 'xss' ]), /string/);  
+ 
+    });
+ 
+    QUnit.test('filter выдает исключение, если справа не массив', function (assert) {
+        const input = 'какая-то строка';
+ 
+        assert.throws(() => filter(input, 123456), /Array/);    
+    });
+ 
+    QUnit.test('filter выдает исключение, если в массиве содержатся не только строки', function (assert) {
+        const input = 'какая-то строка';
+ 
+        assert.throws(() => filter(input, [ 'script', 2, 3, 'doctype' ]), /strings/);
+    });
+ 
 });
