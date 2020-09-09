e5226c8bf8fb51179a871110966208e77eec747d
rufa-a <roofinda@gmail.com>
2020-09-09 11:14:57 +0300

    removed isvalidobject func, added doс

--- source/zip.js
+++ source/zip.js
@@ -1,6 +1,16 @@
 'use strict';
 
 /**
+ * This is an isValidObject function to check whether an object is valid.
+ * @param {object} object - Object from array of objects
+ * @return {boolean} - Checking valid object 
+ * @example
+ *     isValidObject({age: '42'})
+ */
+const isValidObject = (object) =>
+    String(object) === '[object Object]';
+
+/**
  * This is a zip function to combine all properties from all objects remaining first property among repeating properties.
  * @param {object[]} objects - Array of objects
  * @return {object} ObjectsProps - Object with properties of all objects
@@ -9,8 +19,6 @@
  */
 const zip = (...objects) =>
     objects.reduce((iProps, iObject) => {
-        let isValidObject = (object) =>
-            ((String(object)) === ('[object Object]'));
         if (isValidObject(iObject)) {
             Object.entries(iObject).forEach(([key, value]) => {
                 if (!iProps.hasOwnProperty(key)) {
--- tests/zip.js
+++ tests/zip.js
@@ -76,29 +76,29 @@ QUnit.module('Тестируем функцию zip', function () {
 	});
 
 	QUnit.test('Функция правильно работает с вложенными объектами', function (assert) {
-		assert.deepEqual(zip({answer: { age: "42" }}, {age: "42"}), {answer: { age: "42" }, age: "42"});
+		assert.deepEqual(zip({answer: { age: 42 }}, {age: 42}), {answer: { age: 42 }, age: 42});
 
 		const obj = {
 			answer: {
-				 age: {tens: "40", units: "2"} 
+				 age: {tens: 40, units: 2} 
 				}
 		};
-		assert.deepEqual(zip(obj, {answer: "42"}), obj);
+		assert.deepEqual(zip(obj, {answer: 42}), obj);
 
-		let id = Symbol("answer");
-		assert.deepEqual(zip({id: { age: {number: "42"} }}, {answer: "42"}), {id: { age: {number: "42"} }, answer: "42"});
+		let id = Symbol('answer');
+		assert.deepEqual(zip({id: { age: {number: 42} }}, {answer: 42}), {id: { age: {number: 42} }, answer: 42});
 	});
 
 	QUnit.test('Функция правильно работает со свойствами, значения которых null или undefined', function (assert) {
 		assert.deepEqual(zip({name: undefined, value: null}), {name: undefined, value: null});
-		assert.deepEqual(zip({name: undefined, value: "42"}, {name: 'age', value: null}), {name: undefined, value: "42"});
-		assert.deepEqual(zip({name: null, value: "42"}, {name: 'age', value: undefined}), {name: null, value: "42"});
+		assert.deepEqual(zip({name: undefined, value: 42}, {name: 'age', value: null}), {name: undefined, value: 42});
+		assert.deepEqual(zip({name: null, value: 42}, {name: 'age', value: undefined}), {name: null, value: 42});
 	});
 
 	QUnit.test('Некорректный ввод', function (assert) {
-		assert.throws(() => zip("age"), TypeError,  'Invalid type');
+		assert.throws(() => zip('age'), TypeError,  'Invalid type');
 		assert.throws(() => zip(123), TypeError,  'Invalid type');
 		assert.throws(() => zip(new Boolean()), TypeError,  'Invalid type');
-		assert.throws(() => zip(null, {}, {age: "42"}), TypeError,  'Invalid type');
+		assert.throws(() => zip(null, {}, {age: 42}), TypeError,  'Invalid type');
 	});
 });
