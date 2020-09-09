2cb0b9ecd4ff88f73a3cfde414cb1c7489558942
thePsina <mishele20@mail.ru>
2020-09-09 21:25:58 +0300

    add new test and fix code

--- source/sort.js
+++ source/sort.js
@@ -1,22 +1,34 @@
 'use strict'
 
-let sort = (str) => {
-    if (!str || typeof str !== 'string') {
-        return ''
+/**
+ *
+ * @param str
+ * @example
+ * sort('i love frontend')
+ * // returns 'Defnnort Elov I'
+ * @returns {string} Returns sorted words in string with uppercase letter in every word
+ */
+
+const sort = (str) => {
+    if (typeof str !== 'string') {
+        throw new TypeError(`fail type: ${typeof str}`)
+    }
+    if (!str) {
+        throw new SyntaxError(`fail syntax: "${str}"`)
     }
 
-    let words = str.replace(/ +/g, ' ').trim().split(' ')
+    const words = str.trim().split(/\s+/)
 
-    let collator = new Intl.Collator();
+    const collator = new Intl.Collator();
 
-    words = words.map((element) => {
-        element = element.split('').sort((a, b) => {
-            return collator.compare(a, b)
-        }).join('').toLowerCase().split('')
+    const sortedWordsLikeArray = words.map((element) => {
+        element = element.split('').sort((a, b) =>
+            collator.compare(a, b)).join('').toLowerCase().split('')
 
         element[0] = element[0].toUpperCase()
         return element.join('')
     });
 
-    return words.sort().join(' ')
+    return sortedWordsLikeArray.sort((a, b) =>
+        collator.compare(a, b)).join(' ')
 }
--- tests/sort.js
+++ tests/sort.js
@@ -1,47 +1,50 @@
 'use strict';
 
 QUnit.module('Тестируем функцию sort', function () {
-	QUnit.test('Функция делает первую букву слова прописной', function (assert) {
-		assert.strictEqual(sort('яяя'), 'Яяя', 'Работает с русским алфавитом');
-		assert.strictEqual(sort('Бббббб'), 'Бббббб');
-		assert.strictEqual(sort('zzzzzz'), 'Zzzzzz', 'Работает с английским алфавитом');
-		assert.strictEqual(sort('Rrrrrrrr'), 'Rrrrrrrr');
-	});
+    QUnit.test('Функция делает первую букву слова прописной', function (assert) {
+        assert.strictEqual(sort('яяя'), 'Яяя', 'Работает с русским алфавитом');
+        assert.strictEqual(sort('Бббббб'), 'Бббббб');
+        assert.strictEqual(sort('zzzzzz'), 'Zzzzzz', 'Работает с английским алфавитом');
+        assert.strictEqual(sort('Rrrrrrrr'), 'Rrrrrrrr');
+    });
 
-	QUnit.test('Функция делает все буквы, кроме первой, строчными', function (assert) {
-		assert.strictEqual(sort('ЯЯЯЯ'), 'Яяяя', 'Работает с русским алфавитом');
-		assert.strictEqual(sort('zZzZZzzZZZ'), 'Zzzzzzzzzz', 'Работает с английским алфавитом');
-	});
+    QUnit.test('Функция делает все буквы, кроме первой, строчными', function (assert) {
+        assert.strictEqual(sort('ЯЯЯЯ'), 'Яяяя', 'Работает с русским алфавитом');
+        assert.strictEqual(sort('zZzZZzzZZZ'), 'Zzzzzzzzzz', 'Работает с английским алфавитом');
+    });
 
-	QUnit.test('Функция работает с предложениями', function (assert) {
-		assert.strictEqual(sort('ЯЯЯ яяя яяя яяя'), 'Яяя Яяя Яяя Яяя');
-		assert.strictEqual(sort('яяя яяяяя ЯЯЯЯ ЯяяяЯЯЯяя'), 'Яяя Яяяя Яяяяя Яяяяяяяяя');
-	});
+    QUnit.test('Функция работает с предложениями', function (assert) {
+        assert.strictEqual(sort('ЯЯЯ яяя яяя яяя'), 'Яяя Яяя Яяя Яяя');
+        assert.strictEqual(sort('яяя яяяяя ЯЯЯЯ ЯяяяЯЯЯяя'), 'Яяя Яяяя Яяяяя Яяяяяяяяя');
+    });
 
-	QUnit.test('Функция сортирует буквы в отдельных словах по алфавиту', function (assert) {
-		assert.strictEqual(sort('fedcba'), 'Abcdef', 'Работает с английским алфавитом');
-		assert.strictEqual(sort('zyxwvu'), 'Uvwxyz');
-		assert.strictEqual(sort('жёедгвба'), 'Абвгдеёж', 'Работает с русским алфавитом');
-		assert.strictEqual(sort('вбава'), 'Аабвв');
-	});
+    QUnit.test('Функция сортирует буквы в отдельных словах по алфавиту', function (assert) {
+        assert.strictEqual(sort('fedcba'), 'Abcdef', 'Работает с английским алфавитом');
+        assert.strictEqual(sort('zyxwvu'), 'Uvwxyz');
+        assert.strictEqual(sort('жёедгвба'), 'Абвгдеёж', 'Работает с русским алфавитом');
+        assert.strictEqual(sort('вбава'), 'Аабвв');
+    });
 
-	QUnit.test('Функция сортирует слова в предложении по алфавиту', function (assert) {
-		assert.strictEqual(sort('f e d c b a'), 'A B C D E F', 'Работает с английским алфавитом');
-		assert.strictEqual(sort('z y x w v u'), 'U V W X Y Z');
-		assert.strictEqual(sort('в б а в а'), 'А А Б В В');
-	});
+    QUnit.test('Функция сортирует слова в предложении по алфавиту', function (assert) {
+        assert.strictEqual(sort('f e d c b a'), 'A B C D E F', 'Работает с английским алфавитом');
+        assert.strictEqual(sort('z y x w v u'), 'U V W X Y Z');
+        assert.strictEqual(sort('в б а в а'), 'А А Б В В');
+    });
 
-	QUnit.test('Функция правильно обрабатывает некорректные данные', function (assert) {
-		assert.strictEqual(sort(''), '');
-		assert.strictEqual(sort(Math.random()), '');
-		assert.strictEqual(sort(null), '');
-		assert.strictEqual(sort(undefined), '');
-	});
+    QUnit.test('Функция правильно обрабатывает некорректные данные', function (assert) {
+        assert.throws(() => sort(''), SyntaxError(`fail syntax: ""`));
+        assert.throws(() => sort(Math.random()), TypeError(`fail type: ${typeof Math.random()}`));
+    });
 
-	QUnit.test('Функция работает правильно', function (assert) {
-		assert.strictEqual(sort('мама мыла раму'), 'Аамм Алмы Амру');
-		assert.strictEqual(sort('космический корабль летит на марс'), 'Абклорь Амрс Ан Еиийккмоссч Еилтт');
-		assert.strictEqual(sort('i love frontend'), 'Defnnort Elov I');
-		assert.strictEqual(sort('hello world'), 'Dlorw Ehllo');
-	});
+    QUnit.test('Функция работает с русским и английским алфовитом одновременно', function (assert) {
+        assert.strictEqual(sort('ab аб'), 'Аб Ab');
+        assert.strictEqual(sort('космический корабль i love frontend'), 'Абклорь Еиийккмоссч Defnnort Elov I');
+    });
+
+    QUnit.test('Функция работает правильно', function (assert) {
+        assert.strictEqual(sort('мама мыла раму'), 'Аамм Алмы Амру');
+        assert.strictEqual(sort('космический корабль летит на марс'), 'Абклорь Амрс Ан Еиийккмоссч Еилтт');
+        assert.strictEqual(sort('i love frontend'), 'Defnnort Elov I');
+        assert.strictEqual(sort('hello world'), 'Dlorw Ehllo');
+    });
 });
