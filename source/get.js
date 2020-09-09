1a68ee9ed592a91321b09fcb9e421e006159aae2
arugaf <arug113@gmail.com>
2020-09-09 15:17:44 +0300

    chaining style fix

--- source/get.js
+++ source/get.js
@@ -2,7 +2,7 @@
 
 /**
  * @description function get() takes 2 arguments: object and string with property
- *              returns the value of property or undefined 
+ *              returns the value of property or undefined
  *
  * @param {object} object, the object of which we want to get a specific property
  * @param {string} property, string that describes the path to property separated by commas
@@ -14,6 +14,9 @@ const get = (object, property) => {
         throw Error('Incorrect type');
     }
 
-    return property.split('.').filter((str) => str).reduce((result, key) =>
-        (result && result[key]) ? result[key] : undefined, object);
+    return property
+            .split('.')
+            .filter((str) => str)
+            .reduce((result, key) =>
+                (result && result[key]) ? result[key] : undefined, object);
 };
