9e49abbf2fed3d5f4a9446ceffde3d76bd22fe01
nathanKith <ntolparov@gmail.com>
2020-09-09 18:46:18 +0300

    jsdoc + valid args

--- source/plain.js
+++ source/plain.js
@@ -1,9 +1,12 @@
-/*
-* Напишите функцию plain, которая будет принимать на вход массив массивов
-* и создавать из них один общий массив. Массивы могут быть любой вложенности
-* */
-
+/**
+ * Create one array from array of arrays.
+ * @param arrays - array of arrays
+ * @returns {[array]}
+ */
 const plain = (arrays) => {
+    if (!Array.isArray(arrays)) {
+        return [];
+    }
     while (arrays.filter((item) => Array.isArray(item)).length) {
         arrays.forEach((item, index, array) => {
             if (Array.isArray(item)) {
