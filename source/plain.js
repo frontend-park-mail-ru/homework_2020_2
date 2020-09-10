2b3e938788d104390a3f8b8f35e076faf2e73a35
nathanKith <ntolparov@gmail.com>
2020-09-10 13:30:00 +0300

    configure jsdoc + add func

--- source/plain.js
+++ source/plain.js
@@ -1,19 +1,25 @@
 /**
  * Create one array from array of arrays.
- * @param arrays - array of arrays
- * @returns {[array]}
+ * @param {Array} arrays - Array of arrays.
+ * @returns {Array} Array without arrays.
  */
 const plain = (arrays) => {
     if (!Array.isArray(arrays)) {
         return [];
     }
-    while (arrays.filter((item) => Array.isArray(item)).length) {
-        arrays.forEach((item, index, array) => {
+    while (hasArrays(arrays)) {
+        arrays.forEach((item, index) => {
             if (Array.isArray(item)) {
-                array.splice(index, 1, ...item);
+                arrays.splice(index, 1, ...item);
             }
         });
     }
     return arrays;
 }
 
+/**
+ * Give information about numbers of arrays in array.
+ * @param {Array} array Some array.
+ * @returns {number} Number of arrays in given array.
+ */
+const hasArrays = (array) => array.filter((item) => Array.isArray(item)).length;
