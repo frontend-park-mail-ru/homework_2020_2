1c7c45c697f3b853fb0da392d038f5de225ab2ce
govyadkin <mgovyadkin@gmail.com>
2020-09-11 20:39:36 +0300

    returned the previous version

--- source/rle.js
+++ source/rle.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const rle = str => {
-    // if (str.length == 0)
-    //     return '';
     if (str.match(/[^A-Za-z]/)) {
         throw Error('Некорректный тип входных данных');
     }
@@ -13,15 +11,10 @@ const rle = str => {
     for (const now of str) {
         if (now != prev) {
             res += prev;
-            while (count > 9) {
-                res += "9";
-                res += prev;
-                count -= 9;
-            }
             if (count > 1) {
                 res += count;
+                count = 1;
             }
-            count = 1;
             prev = now;
         } else {
             ++count;
@@ -32,11 +25,6 @@ const rle = str => {
         res += prev;
     }
 
-    while (count > 9) {
-        res += "9";
-        res += prev;
-        count -= 9;
-    }
     if (count > 1) {
         res += count;
     }
--- tests/rle.js
+++ tests/rle.js
@@ -4,8 +4,8 @@ QUnit.module('Тестируем функцию rle', function () {
 	QUnit.test('rle работает правильно', function (assert) {
 		assert.strictEqual(rle('AAAB'), 'A3B');
 		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
-		assert.strictEqual(rle('AAAAAAAAAAAAA'), 'A9A4');
-		assert.strictEqual(rle('AAAAAAAAAA'), 'A9A');
+		assert.strictEqual(rle('AAAAAAAAAAAAA'), 'A13');
+		assert.strictEqual(rle('AAAAAAAAAA'), 'A10');
 		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
 	});
 
