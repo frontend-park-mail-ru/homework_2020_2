6c0fbb1c88c98bd4fb8382496131a1203aad8dff
govyadkin <mgovyadkin@gmail.com>
2020-09-12 20:12:51 +0300

    added new tests, fixed the program logic

--- source/rle.js
+++ source/rle.js
@@ -1,32 +1,50 @@
 'use strict';
 
+/**
+ * Функция, реализует RLE-сжатие
+ * @param {string} str Входная последовательность
+ * @returns {string}
+ *
+ * @throws {Error('Некорректный тип входных данных')}
+ * @throws {Error('Недопустимые символы в строке')}
+ * @example
+ * // returns 'A3B'
+ * rle('AAAB');
+ * @example
+ * // returns 'A10'
+ * rle('AAAAAAAAAA');
+ */
+
 const rle = str => {
-    if (str.match(/[^A-Za-z]/)) {
+    if (typeof str !== 'string') {
         throw Error('Некорректный тип входных данных');
     }
 
-    let res = '';
-    let prev = str[0];
-    let count = 0;
-    for (const now of str) {
-        if (now != prev) {
-            res += prev;
-            if (count > 1) {
-                res += count;
-                count = 1;
-            }
-            prev = now;
-        } else {
-            ++count;
+    let regexObj = /[0-9]/;
+    if (regexObj.test(str)) {
+        throw Error('Недопустимые символы в строке');
+    }
+
+    let result = (str.split('')).reduce(function (sum, now) {
+        if (now === sum.prev) {
+            sum.count = sum.count + 1;
+            return sum;
         }
+        sum.res += sum.prev;
+        if (sum.count > 1) {
+            sum.res += sum.count;
+            sum.count = 1;
+        }
+        sum.prev = now;
+        return sum;
+    }, {'count': 0, 'prev': str[0], 'res': ''});
+
+    if (result.count >= 1) {
+        result.res += result.prev;
     }
 
-    if (count >= 1) {
-        res += prev;
+    if (result.count > 1) {
+        result.res += result.count;
     }
-
-    if (count > 1) {
-        res += count;
-    }
-    return res;
+    return result.res;
 }
--- tests/rle.js
+++ tests/rle.js
@@ -6,6 +6,7 @@ QUnit.module('Тестируем функцию rle', function () {
 		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
 		assert.strictEqual(rle('AAAAAAAAAAAAA'), 'A13');
 		assert.strictEqual(rle('AAAAAAAAAA'), 'A10');
+		assert.strictEqual(rle('ДЛИННОШЕЕЕ'), 'ДЛИН2ОШЕ3');
 		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
 	});
 
@@ -18,9 +19,14 @@ QUnit.module('Тестируем функцию rle', function () {
 		assert.strictEqual(rle('a'), 'a');
 	});
 
-	QUnit.test('rle работает только на A-Z и a-z', function (assert) {
-		assert.throws(() => rle('6h'), new Error('Некорректный тип входных данных'));
-		assert.throws(() => rle('444445'), new Error('Некорректный тип входных данных'));
-		assert.throws(() => rle('...-;'), new Error('Некорректный тип входных данных'));
+	QUnit.test('rle работает на всем кроме цифр', function (assert) {
+		assert.throws(() => rle('6h'), new Error('Недопустимые символы в строке'));
+		assert.throws(() => rle('444445first'), new Error('Недопустимые символы в строке'));
+	});
+
+	QUnit.test('rle работает правильно с некорректным типом входных данных', function (assert) {
+		assert.throws(() => rle(null), new Error('Некорректный тип входных данных'));
+		assert.throws(() => rle(undefined), new Error('Некорректный тип входных данных'));
+		assert.throws(() => rle([5,5]), new Error('Некорректный тип входных данных'));
 	});
 });
