cc59a795ab18006e71834b647ce1583a686f42cc
Dellvin <dellvin.black@gmail.com>
2020-09-12 12:26:36 +0300

    fix for pull request

--- source/max.js
+++ source/max.js
@@ -2,27 +2,48 @@
 
 const max = numbers => Math.max(...numbers);
 
-function rle(originData) {
-    let compressedData="";
-    if(typeof originData!=="string")return "NOT STRING!";
-    let counter = 1, prevSymbol=originData.charAt(0);
-    for (let i=1; i<originData.length; i++) {
-        if (!isNaN(originData.charAt(i))) return "UNEXPECTED SYMBOL!";
-        if (originData.charAt(i) === prevSymbol) {
-            counter++;
-        } else {
-            compressedData=addToCompressed(compressedData, prevSymbol, counter);
-            counter = 1;
-            prevSymbol=originData.charAt(i);
-        }
+
+/** @description adding symbol to compressed data with checking num of repeating
+ * @param {string} compressed Compressed data
+ * @param {char} symbol Value that we wanted to add
+ * @param {int} counter Number of repeating this symbol
+ * @returns {*} Updated compressed string
+ */
+const addToCompressed = (compressed, symbol, counter) => {
+    compressed += symbol;
+    if (counter !== 1) {
+        compressed += counter;
     }
-    compressedData=addToCompressed(compressedData, prevSymbol, counter)
-    return compressedData;
+    return compressed;
 }
 
-function addToCompressed(compressed, symbol, counter){
-    compressed += symbol;
-    if(counter!==1)
-        compressed += counter;
-    return compressed;
+
+/** @description compressing origin data by algorithm rle
+ * @param originData what we want to compress
+ * @returns {string|*} result of compressing
+ */
+const rle = originData => {
+    let compressedData = '';
+
+    if (typeof originData !== 'string') {
+        throw('NOT STRING!');
+    }
+
+    const reg = new RegExp('#[0-9]+#');
+    if (reg.test(originData)) {
+        throw ('UNEXPECTED SYMBOL!');
+    }
+
+    let counter = 1, prevSymbol = originData.charAt(0);
+    for (let i = 1; i < originData.length; i++) {
+        if (originData[i] === prevSymbol) {
+            counter++;
+        } else {
+            compressedData = addToCompressed(compressedData, prevSymbol, counter);
+            counter = 1;
+            prevSymbol = originData[i];
+        }
+    }
+    compressedData = addToCompressed(compressedData, prevSymbol, counter)
+    return compressedData;
 }
