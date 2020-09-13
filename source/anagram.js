fd51639f7bac5b9d1fc2cd5f4f739e305af9b667
Dmitry <dimkashow27@mail.ru>
2020-09-14 02:14:03 +0300

    check codestyle

--- source/anagram.js
+++ source/anagram.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /**
  * Группирует слова анаграммы
@@ -7,34 +7,34 @@
  * @returns {string[][]}
  */
 const anagram = words => {
-    if (!words) {
-        throw new Error('Argument is required');
+  if (!words) {
+    throw new Error('Argument is required')
+  }
+
+  if (!Array.isArray(words)) {
+    throw new TypeError('Argument not array')
+  }
+
+  if (words.some(word => typeof word !== 'string')) {
+    throw new TypeError('Array contains not only words')
+  }
+
+  const map = new Map()
+
+  words.forEach((word) => {
+    const sorted = word.trim()
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('')
+    if (map.has(sorted)) {
+      map.get(sorted).push(word)
+    } else {
+      map.set(sorted, [word])
     }
+  })
 
-    if (!Array.isArray(words)) {
-        throw new TypeError('Argument not array');
-    }
-
-    if (words.some(word => typeof word !== 'string')) {
-        throw new TypeError('Array contains not only words');
-    }
-
-    const map = new Map();
-
-    words.forEach((word) => {
-        const sorted = word.trim()
-            .toLowerCase()
-            .split('')
-            .sort()
-            .join('');
-        if (map.has(sorted)) {
-            map.get(sorted).push(word);
-        } else {
-            map.set(sorted, [word]);
-        }
-    })
-
-    const answer = Array.from(map.values()).filter(arr => arr.length > 1);
-    answer.forEach(arr => arr.sort());
-    return answer.sort();
+  const answer = Array.from(map.values()).filter(arr => arr.length > 1)
+  answer.forEach(arr => arr.sort())
+  return answer.sort()
 }
