b4e7087721efb05c1106ec541d3446a7fd5a0b55
Myshonkov Maxim <maxim.myshonkov@gmail.com>
2020-09-13 19:06:46 +0300

    Fix style

--- source/solve.js
+++ source/solve.js
@@ -11,22 +11,23 @@
  * @throws {TypeError} Invalid argument type
  * @throws {SyntaxError} Invalid expression syntax
  */
-const solve = (eq, x) => {
-	if (!/^[-+*x0-9() ]+$/.test(eq) || 
-			/(\*|\+|-){2,}/.test(eq) ||
-			/x *x/.test(eq) ||
-			/[x0-9] *[x0-9]/.test(eq)) {
-		throw new SyntaxError("Invalid expression syntax");
+const solve = (expression, arg) => {
+	if (!/^[-+*x0-9() ]+$/.test(expression) || 
+			/(\*|\+|-){2,}/.test(expression) ||
+			/x *x/.test(expression) ||
+			/[x0-9] *[x0-9]/.test(expression)) {
+		throw new SyntaxError('Invalid expression syntax');
     }
-	if (typeof eq !== "string" || typeof x !== "number" || !Number.isInteger(x)) {
-		throw new TypeError("Invalid parameters");
+	if (typeof expression !== 'string' || typeof arg !== 'number' || 
+			!Number.isInteger(arg)) {
+		throw new TypeError('Invalid parameters');
     }
-    if (eq.split("(").length != eq.split(")").length) {
-		throw new SyntaxError("Invalid braces placement");
+    if (expression.split('(').length !== expression.split(')').length) {
+		throw new SyntaxError('Invalid braces placement');
     }
-	let res = new Function("x", `return ${eq}`)(x)
-	if (typeof res !== "number" || !Number.isInteger(res)) {
-		throw new SyntaxError("Invalid expression");
+	let res = new Function('x', `return ${expression}`)(arg)
+	if (typeof res !== 'number' || !Number.isInteger(res)) {
+		throw new SyntaxError('Invalid expression');
 	}
 	return res;
 }
