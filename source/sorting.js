a87b0ac115247612edaab09bea2c8baebf91e751
naysudes <47372468+naysudes@users.noreply.github.com>
2020-09-15 18:09:48 +0300

    Update sorting.js
    исправлена функция sorting

--- source/sorting.js
+++ source/sorting.js
@@ -1,5 +1,5 @@
 'use strict';
-
+ 
 /**
  * Sorting plain-objects array by given keys.
  * 
@@ -15,14 +15,28 @@
  *     sorting({techno: 'park'}, {techno: 'atom'}, [ 'techno' ])
  */
 function sorting(inputArr, keys) {
-    if (!Array.isArray(inputArr)) {
-        return [];
+    if (!Array.isArray(inputArr) || !Array.isArray(keys)) {
+        throw new Error('Args should be arrays');
     }
     keys.reverse().forEach(element => {
-        inputArr.sort(function(a, b) {
-            var isNumber = (typeof a[element] === "number");
-            return a[element].toString().localeCompare(b[element].toString(), 'en-US', { numeric: isNumber });
+        inputArr.sort((a, b) => {
+            if ((typeof a[element]) !== (typeof b[element])) {
+                throw new Error('Comparing elements should same type objects');
+            }
+            if ((typeof a[element]) === "string") {
+                return b[element] - a[element];
+            } else if ((typeof a[element]) === "number") {
+                return a[element] - b[element];
+            } else {
+                throw new Error('Comparing elements should be strings or numbers');
+            }
         });
+ 
+        // inputArr.sort((a, b) => {
+        //     var isNumber = (typeof a[element] === "number");
+        //     return a[element].toString().localeCompare(b[element].toString(), 'en-US', { numeric: isNumber });
+        // });
+        
     });
     return inputArr;
 };
