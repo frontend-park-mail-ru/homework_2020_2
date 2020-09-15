8933f48dddc22970cb4ca7ad1ceb7db9de449cfd
lowsukuku-work <lowsukuku@work.com>
2020-09-15 15:40:49 +0300

    убрал лишние фигурные скобки добавил пробелы

--- tests/anagram.js
+++ tests/anagram.js
@@ -1,7 +1,7 @@
 'use strict';
 
-QUnit.module('Тестируем функцию anagram', function() {
-    QUnit.test('Функция работает правильно', function(assert) {
+QUnit.module('Тестируем функцию anagram', function () {
+    QUnit.test('Функция работает правильно', function (assert) {
         const inputLetters = ['abc', 'abC', 'aBc', 'aBC', 'Abc', 'AbC', 'ABc', 'ABC'];
 
         const input = [
@@ -22,26 +22,26 @@ QUnit.module('Тестируем функцию anagram', function() {
         assert.deepEqual(anagram(inputLetters), [inputLetters.sort()]);
     });
 
-    QUnit.test('Функция работает правильно с некорректными аргументами', function(assert) {
-        assert.throws(() => { anagram(0); }, new TypeError(
+    QUnit.test('Функция работает правильно с некорректными аргументами', function (assert) {
+        assert.throws(() => anagram(0), new TypeError(
             `Expected 'Array', received ${typeof 0}`));
-        assert.throws(() => { anagram(undefined); }, new TypeError(
+        assert.throws(() => anagram(undefined), new TypeError(
             `Expected 'Array', received ${typeof undefined}`));
-        assert.throws(() => { anagram('hello'); }, new TypeError(
+        assert.throws(() => anagram('hello'), new TypeError(
             `Expected 'Array', received ${typeof 'hello'}`));
-        assert.throws(() => { anagram({ a: 1, b: 2 }); }, new TypeError(
+        assert.throws(() => anagram({ a: 1, b: 2 }), new TypeError(
             `Expected 'Array', received ${typeof { a: 1, b: 2 }}`));
 
     });
 
-    QUnit.test('Функция работает правильно с массивом, состоящим не из строк', function(assert) {
-        assert.throws(() => { anagram([0, 0, 0, 0, 0]); }, new TypeError(
+    QUnit.test('Функция работает правильно с массивом, состоящим не из строк', function (assert) {
+        assert.throws(() => anagram([0, 0, 0, 0, 0]), new TypeError(
             `Expected Array elements type of 'string', received element of another type`
         ));
-        assert.throws(() => { anagram(['abc', 'def', 'ghi', 'j', 0]); }, new TypeError(
+        assert.throws(() => anagram(['abc', 'def', 'ghi', 'j', 0]), new TypeError(
             `Expected Array elements type of 'string', received element of another type`
         ));
-        assert.throws(() => { anagram([undefined, 0, null]); }, new TypeError(
+        assert.throws(() => anagram([undefined, 0, null]), new TypeError(
             `Expected Array elements type of 'string', received element of another type`
         ));
     });
