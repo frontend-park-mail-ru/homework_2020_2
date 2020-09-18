afa44ba53a95cf60b34c639462682b948c3d4bfd
naysudes <47372468+naysudes@users.noreply.github.com>
2020-09-18 19:06:31 +0300

    Update sorting.js
    Исправлена функция sorting

--- source/sorting.js
+++ source/sorting.js
@@ -12,32 +12,27 @@
  *
  * @example
  *
- *     sorting({techno: 'park'}, {techno: 'atom'}, [ 'techno' ])
+ *     sorting([{'techno': 'park'}, {'techno': 'atom'}], [ 'techno' ])
  */
-function sorting(inputArr, keys) {
+const sorting = (inputArr, keys) => {
     if (!Array.isArray(inputArr) || !Array.isArray(keys)) {
         throw new Error('Args should be arrays');
     }
+    keys.forEach(key => {
+        const type = typeof inputArr[0][key];
+        if (!inputArr.every(obj => typeof obj[key] === type) && (type === "string" || type === "number")) {
+            throw new Error('Comparing elements should be the same type objects - strings or numbers');
+        }
+    })
     keys.reverse().forEach(element => {
         inputArr.sort((a, b) => {
-            if ((typeof a[element]) !== (typeof b[element])) {
-                throw new Error('Comparing elements should same type objects');
-            }
             if ((typeof a[element]) === "string") {
-                return b[element] - a[element];
-            } else if ((typeof a[element]) === "number") {
-                return a[element] - b[element];
+                return a[element].localeCompare(b[element]);
             } else {
-                throw new Error('Comparing elements should be strings or numbers');
-            }
+                return a[element] - b[element];
+            };
         });
- 
-        // inputArr.sort((a, b) => {
-        //     var isNumber = (typeof a[element] === "number");
-        //     return a[element].toString().localeCompare(b[element].toString(), 'en-US', { numeric: isNumber });
-        // });
-        
     });
     return inputArr;
-};
+}
  
